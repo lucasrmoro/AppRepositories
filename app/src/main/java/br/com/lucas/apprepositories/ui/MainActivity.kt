c1538e1e3075bc2a0a628121e122fddbe41a2558
lucasrmoro <lucas.rmoro1@gmail.com>
2021-10-11 14:13:16 -0300

    Setup menu.

--- app/src/main/java/br/com/lucas/apprepositories/ui/MainActivity.kt
+++ app/src/main/java/br/com/lucas/apprepositories/ui/MainActivity.kt
@@ -2,11 +2,40 @@ package br.com.lucas.apprepositories.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 import br.com.lucas.apprepositories.R
+import br.com.lucas.apprepositories.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        return true
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        return false
+    }
+
+    companion object{
+        private const val TAG = "TAG"
     }
 }
